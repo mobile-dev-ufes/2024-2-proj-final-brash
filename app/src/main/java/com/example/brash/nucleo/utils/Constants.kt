d26a0c4631ae5071a36b0fb58bd79af29a60c6a9
Renzo-Henrique <renzolealguzzo@gmail.com.br>
2025-03-09 15:46:31 -0300

    Melhorias, troca no botão de trocar idioma

--- app/src/main/java/com/example/brash/nucleo/utils/Constants.kt
+++ app/src/main/java/com/example/brash/nucleo/utils/Constants.kt
@@ -1,6 +1,8 @@
 package com.example.brash.nucleo.utils
 
 
-class Constants {
-
+object Constants {
+    const val CHAVE_ACESSO_STR = "CHAVE_ACESSO"
+    const val CHAVE_NOME = "NOME_USUARIO"
+    const val CHAVE_SENHA = "NOME_SENHA"
 }
