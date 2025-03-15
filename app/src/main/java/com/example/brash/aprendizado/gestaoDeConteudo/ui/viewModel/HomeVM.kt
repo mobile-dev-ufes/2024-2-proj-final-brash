d03a622b1158094a43ff22cb20bfced8c0fdbe17
saick123 <matheussaick@gmail.com>
2025-03-15 14:54:44 -0300

    home vm usando corrotinas agora

--- app/src/main/java/com/example/brash/aprendizado/gestaoDeConteudo/ui/viewModel/HomeVM.kt
+++ app/src/main/java/com/example/brash/aprendizado/gestaoDeConteudo/ui/viewModel/HomeVM.kt
@@ -97,54 +97,56 @@ class HomeVM(application: Application) : AndroidViewModel(application) {
             HomeAcListItem.HomeAcPastaItem(pasta = Pasta(nome =  "6")),
             HomeAcListItem.HomeAcBaralhoItem(Baralho(nome = "Fone de ouvido"))
         )
-
-
         _homeAcListItemList.value = listaHomeAcListItem
         */
 
-        pastaRepository.getFolders(
-            onSuccess = { folders ->
-                // A operação foi bem-sucedida, faça algo com as pastas (folders)
-                Log.d("Pasta", "Pastas carregadas: $folders")
+        viewModelScope.launch {
+            val result = pastaRepository.getFolders()
 
-                //TODO:: transformar em homeAcListItem
-                _pastaList.value = folders
+            result
+                .onSuccess {
+                        folders ->
+                    // A operação foi bem-sucedida, faça algo com as pastas (folders)
+                    Log.d("Pasta", "Pastas carregadas: $folders")
 
-                // Criação do homeAcListItem, onde vamos adicionar as pastas e baralhos
-                val homeAcListItem = folders.flatMap { folder ->
-                    if (folder.idPasta == "root") {
-                        // Se for a pasta "root", adicione os baralhos
-                        folder.baralhos.map { baralho ->
-                            HomeAcListItem.HomeAcBaralhoItem(baralho = baralho)
+                    //TODO:: transformar em homeAcListItem
+                    _pastaList.value = folders
+
+                    // Criação do homeAcListItem, onde vamos adicionar as pastas e baralhos
+                    val homeAcListItem = folders.flatMap { folder ->
+                        if (folder.idPasta == "root") {
+                            // Se for a pasta "root", adicione os baralhos
+                            folder.baralhos.map { baralho ->
+                                HomeAcListItem.HomeAcBaralhoItem(baralho = baralho)
+                            }
+                        } else {
+                            // Caso contrário, adicione a pasta
+                            listOf(HomeAcListItem.HomeAcPastaItem(pasta = folder, isExpanded = folder.baralhos.isNotEmpty()))
                         }
-                    } else {
-                        // Caso contrário, adicione a pasta
-                        listOf(HomeAcListItem.HomeAcPastaItem(pasta = folder, isExpanded = folder.baralhos.isNotEmpty()))
-                    }
-                }.toMutableList()
+                    }.toMutableList()
 
-                // Primeiro, ordena as pastas pelo nome
-                val (pastas, baralhos) = homeAcListItem.partition {
-                    it is HomeAcListItem.HomeAcPastaItem
+                    // Primeiro, ordena as pastas pelo nome
+                    val (pastas, baralhos) = homeAcListItem.partition {
+                        it is HomeAcListItem.HomeAcPastaItem
+                    }
+
+                    // Ordena as pastas pelo nome
+                    val sortedPastas = pastas.sortedBy { (it as HomeAcListItem.HomeAcPastaItem).pasta.nome }
+
+                    // Ordena os baralhos pelo nome
+                    val sortedBaralhos = baralhos.sortedBy { (it as HomeAcListItem.HomeAcBaralhoItem).baralho.nome }
+
+                    // Junta as pastas ordenadas com os baralhos, com os baralhos vindo após as pastas
+                    _homeAcListItemList.value = sortedPastas + sortedBaralhos
+                }
+                .onFailure {
+                    Log.e("Pasta", "Erro ao carregar pastas do firebase")
+
+                    _pastaList.value = emptyList()
+                    _homeAcListItemList.value = emptyList()
                 }
 
-                // Ordena as pastas pelo nome
-                val sortedPastas = pastas.sortedBy { (it as HomeAcListItem.HomeAcPastaItem).pasta.nome }
-
-                // Ordena os baralhos pelo nome
-                val sortedBaralhos = baralhos.sortedBy { (it as HomeAcListItem.HomeAcBaralhoItem).baralho.nome }
-
-                // Junta as pastas ordenadas com os baralhos, com os baralhos vindo após as pastas
-                _homeAcListItemList.value = sortedPastas + sortedBaralhos
-            },
-            onFailure = {
-                // A operação falhou, trate o erro
-                Log.e("Pasta", "Erro ao carregar pastas do firebase")
-
-                _pastaList.value = emptyList()
-                _homeAcListItemList.value = emptyList()
-            }
-        )
+        }
 
     }
 
